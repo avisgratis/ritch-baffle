c4177601be607bd637282b43ddbab9e7d9b7d363
Avisgratis <avisgratis@gmail.com>
2016-11-21 22:33:15 +0000

    Added basic functionality

---
+++ ritch-baffle.js
@@ -0,0 +1,12 @@
+RitchBaffle = {
+    names: [],
+    
+    addName: function(name) {
+        RitchBaffle.names.push(name);
+    },
+    
+    showNames: function() {
+        alert(RitchBaffle.names);
+    }
+    
+}
